d037b145cd4b579846e4aaaf37b9b3e808026370
unknown <alfredo.goeten@outlook.com>
2022-03-02 10:54:25 -0300

    dependencies added

--- src/run-env.py
+++ src/run-env.py
@@ -14,10 +14,11 @@ pip2 install netifaces
 pip2 install backports.ssl_match_hostname
 pip2 install cryptography
 sudo apt-get install libssl-dev libffi-dev
+pip2 install python-magic
 sudo apt-get install libmagic1
 '''
 
 process = subprocess.Popen(
     '/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-out, err = process.communicate(commands)
-print out
+out, err = process.communicate(commands.encode('utf-8'))
+print (out.decode('utf-8'))
